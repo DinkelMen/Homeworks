69536b1cbb48a589054dd077de6c2cf2f662b857
Dima <bdv09mail@gmail.com>
2022-04-13 12:08:21 +0300

    Добавлены ДЗ

---
+++ Borodin D. lesson 2.py
@@ -0,0 +1,22 @@
+# 3 переменные с одинаковым данными и ID
+item1 = '123'
+item2 = '123'
+item3 = '123'
+print(id(item1)==id(item2)==id(item3))
+
+# 2 переменные с одинаковыми данными с разным ID
+item5 = 123
+item6 = '123'
+print(id(item5)==id(item6))
+
+# У 1х трёх разные идентификаторы, а у 2х последних одинаковые
+item7 = '123'
+item8 = 123
+item9 = 123.0
+print(id(item7)==id(item8))
+print(id(item8)==id(item9))
+print(id(item7)==id(item9))
+
+item10 = '123'
+item11 = '123'
+print(id(item10)==id(item11))
