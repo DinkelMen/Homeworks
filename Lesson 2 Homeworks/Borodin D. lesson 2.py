bf1bc11d07b38ba6e160874206914cfc19c2cd9b
Dima <bdv09mail@gmail.com>
2022-07-08 17:14:05 +0300

    Changes

--- Lesson 2 Homeworks/Borodin D. lesson 2.py
+++ Lesson 2 Homeworks/Borodin D. lesson 2.py
@@ -2,21 +2,21 @@
 item1 = '123'
 item2 = '123'
 item3 = '123'
-print(id(item1)==id(item2)==id(item3))
+print(id(item1) == id(item2) == id(item3))
 
 # 2 переменные с одинаковыми данными с разным ID
-item5 = 123
-item6 = '123'
-print(id(item5)==id(item6))
+item5 = [123]
+item6 = [123]
+print(id(item5) == id(item6))
 
-# У 1х трёх разные идентификаторы, а у 2х последних одинаковые
-item7 = '123'
-item8 = 123
-item9 = 123.0
-print(id(item7)==id(item8))
-print(id(item8)==id(item9))
-print(id(item7)==id(item9))
+# У первых трёх переменных разные идентификаторы, а у 2х последних одинаковые(При одинаковых данных)
+item7 = [123]
+item8 = [123]
+item9 = [123]
+print(id(item7) == id(item8))
+print(id(item8) == id(item9))
+print(id(item7) == id(item9))
 
-item10 = '123'
-item11 = '123'
-print(id(item10)==id(item11))
+item10 = 123
+item11 = 123
+print(id(item10) == id(item11))
